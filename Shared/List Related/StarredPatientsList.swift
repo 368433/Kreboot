206b28fd429dc18d6ad003c25242c3a19eb9023e
AI-Orion <amir.ibrhm@gmail.com>
2021-01-31 13:10:34 -0500

    separated filtered list enum

---
+++ Shared/List Related/StarredPatientsList.swift
@@ -0,0 +1,42 @@
+//
+//  StarredPatientsList.swift
+//  Kreboot
+//
+//  Created by Amir Mac Pro 2019 on 2021-01-31.
+//
+
+import SwiftUI
+
+struct StarredPatientsList: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.presentationMode) var presentationMode
+    
+//    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \PatientsList.title, ascending: true)], animation: .default)
+    @FetchRequest(entity: PatientsList.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \PatientsList.title, ascending: true)], predicate: ListFilterEnum.favorite.predicate, animation: .default)
+    private var lists: FetchedResults<PatientsList>
+    
+    @State private var presentForm: Bool = false
+    @State private var listGroup: ListFilterEnum = .favorite
+    
+    var body: some View {
+        List(lists) { list in
+            NavigationLink(destination: PatientListDetailView(list: list)){
+                ListRow(list: list)
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .principal){
+                Text("Lists").font(.largeTitle).fontWeight(.bold)
+            }
+            ToolbarItem(placement: .primaryAction){
+                Button(action: {presentForm.toggle()}){Image(systemName: "plus")}
+            }
+        }.sheet(isPresented: $presentForm, content: {ListFormView()})
+    }
+}
+
+struct StarredPatientsList_Previews: PreviewProvider {
+    static var previews: some View {
+        StarredPatientsList()
+    }
+}
