7ac146aedf93450456af356c44f0eac485fd542e
AI-Orion <amir.ibrhm@gmail.com>
2021-02-01 21:40:44 -0500

    added ICD10code object to core data graph

---
+++ Shared/ICDImport  and view/ICDdbTable.swift
@@ -0,0 +1,20 @@
+//
+//  ICDdbTable.swift
+//  Kreboot (iOS)
+//
+//  Created by Amir Mac Pro 2019 on 2021-02-01.
+//
+
+import SwiftUI
+
+struct ICDdbTable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ICDdbTable_Previews: PreviewProvider {
+    static var previews: some View {
+        ICDdbTable()
+    }
+}
