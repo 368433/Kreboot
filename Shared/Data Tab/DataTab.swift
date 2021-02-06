e07b9f24fc285360f66775a22fa14a530f7436c6
AI-Orion <amir.ibrhm@gmail.com>
2021-02-05 20:24:35 -0500

    reorganized files and added dataTab view file

---
+++ Shared/Data Tab/DataTab.swift
@@ -0,0 +1,20 @@
+//
+//  DataTab.swift
+//  Kreboot (iOS)
+//
+//  Created by Amir Mac Pro 2019 on 2021-02-05.
+//
+
+import SwiftUI
+
+struct DataTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DataTab_Previews: PreviewProvider {
+    static var previews: some View {
+        DataTab()
+    }
+}
