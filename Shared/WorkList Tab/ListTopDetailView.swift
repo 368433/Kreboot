6b400b1f8a80e892ff37bf559073d60286b905bc
AI-Orion <amir.ibrhm@gmail.com>
2021-02-07 07:31:11 -0500

    renamed list related group to worklist tab. Cleaned up code in detail list view

---
+++ Shared/WorkList Tab/ListTopDetailView.swift
@@ -0,0 +1,31 @@
+//
+//  ListTopDetailView.swift
+//  Kreboot
+//
+//  Created by Amir Mac Pro 2019 on 2021-02-07.
+//
+
+import SwiftUI
+
+struct ListTopDetailView: View {
+    var list: PatientsList
+    var editAction: ()->Void
+    
+    var body: some View{
+        HStack(alignment:.center){
+            Text(list.wrappedTitle).font(.title).fontWeight(.heavy).lineLimit(3)
+            VStack (alignment:.leading){
+                Text("Semaine du " + list.dayLabel(dateStyle: .medium)).font(.footnote)
+                Text("some other useful information").font(.footnote)
+            }.lineLimit(2).foregroundColor(.secondary)
+            Spacer()
+            Button(action: editAction){Text("Edit").font(.caption)}.buttonStyle(CapsuleButton())
+        }.padding(.bottom)
+    }
+}
+
+struct ListTopDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListTopDetailView(list: PersistenceController.singleList, editAction: {})
+    }
+}
--- Shared/WorkList Tab/PatientListDetailView.swift
+++ Shared/WorkList Tab/PatientListDetailView.swift
@@ -16,14 +16,12 @@ struct PatientListDetailView: View {
     var body: some View {
         ScrollView {
             VStack(alignment:.leading){
-                TopDetailView(listTitle: list.wrappedTitle, dayLabel: list.dayLabel(dateStyle: .medium), editAction: {showEditListForm.toggle()})
-                
+                ListTopDetailView(list: list, editAction: {showEditListForm.toggle()})
                 VStack {
                     ForEach(list.patientsArray, id:\.self){ patient in
                         PatientRow2(patient: patient)
                     }
                 }
-                Spacer()
             }.padding()
             .navigationBarTitleDisplayMode(.inline)
             .toolbar{
@@ -44,29 +42,8 @@ struct PatientListDetailView: View {
                 NavigationView{ListFormView(list: list)}
             })
         }
-        
     }
 }
-
-struct TopDetailView: View {
-    var listTitle: String
-    var dayLabel: String
-    var editAction: ()->Void
-
-    
-    var body: some View{
-        HStack(alignment:.center){
-            Text(listTitle).font(.title).fontWeight(.heavy).lineLimit(3)
-            VStack (alignment:.leading){
-                Text("Semaine du " + dayLabel).font(.footnote)
-                Text("some other useful information").font(.footnote)
-            }.lineLimit(2).foregroundColor(.secondary)
-            Spacer()
-            Button(action: editAction){Text("Edit").font(.caption)}.buttonStyle(CapsuleButton())
-        }.padding(.bottom)
-    }
-}
-
 
 struct PatientListDetailView_Previews: PreviewProvider {
     static var previews: some View {
