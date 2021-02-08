67f91fec422caa5856f8bd9ff59ef37476fbed60
AI-Orion <amir.ibrhm@gmail.com>
2021-02-08 03:43:38 -0500

    removed dead code from listformview

--- Shared/WorkList Tab/ListFormView.swift
+++ Shared/WorkList Tab/ListFormView.swift
@@ -11,40 +11,25 @@ struct ListFormView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @Environment(\.presentationMode) private var presentationMode
     
-    var list: PatientsList?
-    var defaultStar: Bool
-    var disableSave: Bool { return title == "" }
+    @ObservedObject var list: PatientsList
+    var disableSave: Bool { return list.title?.isEmpty ?? true }
     
-    @State var title: String
-    @State var listDescription: String
-    @State var isArchived: Bool
-    @State var isFavorite: Bool
-    @State var dateCreated: Date
-    
-    init(list: PatientsList? = nil, starred: Bool = false ){
-        self.defaultStar = starred
-        self.list = list
-        self._title = State(initialValue: list?.title ?? "")
-        self._listDescription = State(initialValue: list?.listDescription ?? "")
-        self._dateCreated = State(initialValue: list?.dateCreated ?? Date())
-        self._isArchived = State(initialValue: list?.isArchived ?? false)
-        self._isFavorite = State(initialValue: starred ? true:(list?.isFavorite ?? false))
+    init(list: PatientsList? = nil ){
+        self.list = list ?? PatientsList(context: PersistenceController.shared.container.viewContext)
     }
     
     var body: some View {
         List{
-            TextField("Title", text: $title)
-                .labeledTF(label: "Title", isEmpty: title == "")
-            ScrollChoice(labelText: $title, choice: SomeConstants.listTitleChoice )
-            TextField("List description", text: $listDescription)
-                .labeledTF(label: "List description", isEmpty: listDescription == "")
+            TextField("Title", text: $list.title ?? "")
+            ScrollChoice(labelText: $list.title ?? "", choice: SomeConstants.listTitleChoice )
+            TextField("List description", text: $list.listDescription ?? "")
             HStack {
                 Text("Favorite")
                 Spacer()
-                Button(action: {isFavorite.toggle()}){Image(systemName: isFavorite ? "star.fill":"star")}
+                Button(action: {list.isFavorite.toggle()}){Image(systemName: list.isFavorite ? "star.fill":"star")}
             }
-            Toggle(isOn: $isArchived){Text("Archive")}.padding(.trailing)
-            DatePicker("Date created", selection: $dateCreated, displayedComponents: .date)            
+            Toggle(isOn: $list.isArchived){Text("Archive")}.padding(.trailing)
+            DatePicker("Date created", selection: $list.dateCreated ?? Date(), displayedComponents: .date)
         }
         .navigationBarTitleDisplayMode(.inline)
         .toolbar{
@@ -56,16 +41,7 @@ struct ListFormView: View {
     }
     
     private func Save() -> Void {
-        // TODO Add message to confirm overwriting changes
-        guard title != "" else { return }
-        
-        let listToSave = list ?? PatientsList(context: viewContext)
-        listToSave.title = self.title
-        listToSave.listDescription = self.listDescription
-        listToSave.dateCreated = self.dateCreated
-        listToSave.isFavorite = self.isFavorite
-        listToSave.isArchived = self.isArchived
-        listToSave.saveYourself(in: viewContext)
+        list.saveYourself(in: viewContext)
         self.presentationMode.wrappedValue.dismiss()
     }
 }
