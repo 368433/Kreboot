7865579b785173ad0c90ea98a98c28c3cc69141b
AI-Orion <amir.ibrhm@gmail.com>
2021-02-28 08:32:55 -0500

    resave

--- Shared/Views/ActFormView.swift
+++ Shared/Views/ActFormView.swift
@@ -7,35 +7,6 @@
 
 import SwiftUI
 
-class AddActViewModel: ObservableObject {
-    @Published var searchActText: String = ""
-    @Published var actCode: String = ""
-    @Published var actDate: Date = Date()
-    @Published var historyNote: String = ""
-    @Published var physicalExamNote = ""
-    @Published var consultingPhysician = ""
-    private var episode: MedicalEpisode?
-    private var act: Act?
-    
-    init(act: Act?, episode: MedicalEpisode?){
-        self.episode = episode
-        self.act = act
-        
-        self._actCode = Published(initialValue: self.act?.ramqCode ?? "")
-        self._actDate = Published(initialValue: self.act?.timestamp ?? Date())
-    }
-    
-    func saveAct(){
-        let context = PersistenceController.shared.container.viewContext
-        let actToSave = act ?? Act(context: context)
-        actToSave.ramqCode = self.actCode
-        actToSave.timestamp = self.actDate
-        if let episode = episode {
-            actToSave.medicalEpisode = episode
-        }
-        actToSave.saveYourself(in: context)
-    }
-}
 
 
 struct ActFormView: View {
