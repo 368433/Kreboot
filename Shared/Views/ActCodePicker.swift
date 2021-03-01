1890b240d32531f2aa2faf3cbfef5a77608aeed7
AI-Orion <amir.ibrhm@gmail.com>
2021-03-01 17:53:18 -0500

    trying to find good data representation for act code picker

--- Shared/Views/ActCodePicker.swift
+++ Shared/Views/ActCodePicker.swift
@@ -18,49 +18,25 @@ struct ActCodePicker: View {
     
     var body: some View {
         HStack(alignment: .top){
-            VStack{
-                ForEach(selectedOption, id:\.self) { item in
-                    Text(item)
-                }
-            }
-            Divider()
-            ScrollView(showsIndicators: false){
-                LazyHGrid(rows: rows, content: {
-                    ForEach(codeOptions){ code in
-                        Text(code.code)
-                            .onTapGesture {
-                                selectedOption.append(code.code)
-                            }
-                    }
-                })
-            }
-        }.frame(height: 200)
+            
+        }
     }
 }
 
-struct test2: View {
-    var codeOptions: [BillCodeElement] = ["HPB", "ICM", "MGH", "GHJ", "FSDF"].map{BillCodeElement(code: $0)}
-    @State var selectedOption: BillCodeElement
-    @State var tet = testing.test2
-    
-    init(){
-        let test = codeOptions.first!
-        self._selectedOption = State(initialValue: test)
-    }
-    
-    var body: some View{
-        Picker("test", selection: $selectedOption){
-            ForEach(codeOptions) { option in
-                Text(option.code).tag(option)
-            }
-        }.pickerStyle(SegmentedPickerStyle())
-    }
+struct ramqAct{
+    var inOutPatient: String
+    var building: String
+    var actCategory: String
+    var actType: String
+    var code: String
+    var fee: Double
 }
 
-enum testing: CaseIterable, Identifiable {
-    case test1, test2
-    var id: Int { hashValue }
-}
+enum InOutPatient { case inPatient, outPatient }
+enum Building { case hospital, cabinetOutpt, urgence, cliniExt, autres }
+enum ActCategory { case rout, miee, crit, opat, iNoso, transf, prophylaxis, outbreak, expoBio }
+enum ActType { case vp, vc, c, vt, tw, planif, eval, eMajj1, eMajjsubseq }
+
 
 struct BillCodeElement: Identifiable, Hashable {
     var id = UUID()
@@ -70,11 +46,5 @@ struct BillCodeElement: Identifiable, Hashable {
 struct ActCodePicker_Previews: PreviewProvider {
     static var previews: some View {
         ActCodePicker()
-    }
-}
-
-struct test2_Previews: PreviewProvider {
-    static var previews: some View {
-        test2()
     }
 }
